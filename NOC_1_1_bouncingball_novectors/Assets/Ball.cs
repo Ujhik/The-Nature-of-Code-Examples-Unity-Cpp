9f62f75c849481d6d13e41d18bc0dc9abea26d6e
Ramiro Sánchez Torres <ujhik@usal.es>
2018-12-08 00:54:06 +0100

    NOC_1_1_bouncingball_novectors finished

---
+++ NOC_1_1_bouncingball_novectors/Assets/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] float speed;
+
+    float radius;
+    Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+        direction = Vector2.one.normalized; //Direction is (1,1) normalized
+        radius = transform.localScale.x / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        //Rebound bottom
+        if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0)
+        {
+            direction.y = -direction.y;
+        }
+        
+        //Rebound top
+        if (transform.position.y > GameManager.topRight.y - radius && direction.y > 0)
+        {
+            direction.y = -direction.y;
+        }
+
+        //Rebound left
+        if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0)
+        {
+            direction.x = -direction.x;
+        }
+
+        //Rebound right
+        if (transform.position.x > GameManager.topRight.x - radius && direction.x > 0)
+        {
+            direction.x = -direction.x;
+        }
+
+
+    }
+}
---
+++ NOC_1_1_bouncingball_novectors/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Ball ball;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+
+
+    // Use this for initialization
+    void Start () {
+
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+
+
+        //Create ball
+        Instantiate(ball);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+} 
